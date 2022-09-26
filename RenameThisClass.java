5c2612f2b0db4442e0734369b63b4b6f86e25d33
Noelle Barragan <noelle.barragan@mrjaffesclass.com>
2022-09-26 14:08:59 -0700

    first commit

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,6 +8,6 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("git practice, god im behind help please.");
     }
 }
