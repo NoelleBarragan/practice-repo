66fd91e7d61e2c97bba5b9b5d519f65e3227bc46
Noelle Barragan <noelle.barragan@mrjaffesclass.com>
2022-09-27 14:23:19 -0700

    Array Save

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,6 +8,13 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
-        System.out.println("git practice, god im behind help please.");
+        String[] StringArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
+        int i = 0;
+        for (i = 0; i < StringArray.length; i++) {
+            System.out.println(StringArray[i]);
+            i++;
+        }
+
+        
     }
 }
